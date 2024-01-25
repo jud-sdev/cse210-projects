97403f58fdd497a5ff5e9973f95dd34429dc0270
jude-sdev <judmc.26@gmail.com>
2024-01-25 14:09:24 +0100

    Wrote a c# class to compute fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,30 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int top)
+    {
+        _numerator = top;
+        _denominator = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _numerator = top;
+        _denominator = bottom;
+    }
+    public string GetFractionString()
+    {
+        string text = $"{_numerator}/{_denominator}";
+        return text;
+    }
+    public double GetDecimalValue()
+    {
+         return (double)_numerator / (double)_denominator;
+    }
+   
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstFraction = new Fraction();
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+
+        Fraction secondFraction = new Fraction(5);
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+
+        Fraction thridFraction = new Fraction(3, 4);
+        Console.WriteLine(thridFraction.GetFractionString());
+        Console.WriteLine(thridFraction.GetDecimalValue());
+
+        Fraction fourthFraction = new Fraction(1, 3);
+        Console.WriteLine(fourthFraction.GetFractionString());
+        Console.WriteLine(fourthFraction.GetDecimalValue());
     }
 }
