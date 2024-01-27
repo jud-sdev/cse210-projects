30c4f9a247351aa612525dee659423fe97501d48
jude-sdev <judmc.26@gmail.com>
2024-01-28 00:22:07 +0100

    wrote scripture memorizer program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string response = "yes";
+        while (response.ToLower() == "yes")
+        {
+            Scripture scripture = new Scripture("1 Nephi ", 3, 17, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them. ");
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nCongratulations!!! You've memorized all of the words in this scripture.");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+            Console.Write("\nWould you like to repeat the scripture mastery? (yes or no) ");
+            response = Console.ReadLine();
+
+        }
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,26 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+}
