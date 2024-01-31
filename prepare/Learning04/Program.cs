b6e67921adc6da63fd5134cc2d8fca81e007c713
jude-sdev <judmc.26@gmail.com>
2024-02-01 00:56:34 +0100

    implemented inheritance principles in my classes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment firstAssg = new Assignment("Ebigide Jude", "Multiplication");
+        string summary = firstAssg.GetSummary();
+        Console.WriteLine(summary);
+        
+        MathAssignment mathematics = new MathAssignment("Ebigide Jude", "Algebra", "5.2", "6-12");
+        Console.WriteLine(mathematics.GetSummary());
+        Console.WriteLine(mathematics.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Ebigide Jude", "Nigerian History", "The Joy of Independence day");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
